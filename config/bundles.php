366c132e3ec5137d8f9aac74f60df60a7b7b0d99
Tomasz Sobijanek <tsobijanek@gmail.com>
2023-03-08 18:58:06 +0100

    env configuration

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true]
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
